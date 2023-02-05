def4e355c27bdc26148a63472c524850eae61a1c
MargaridaFCosta <margaridaxavicosta@hotmail.com>
2023-02-05 17:45:23 +0000

    Add bootstrap and css styles

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,21 +1,30 @@
 import React from "react";
-
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
-  return (
-    <div className="row">
-      <div className="col-6">
-        <h2>{props.data.city}</h2>
-        <p> {Math.round(props.data.temperature)} ºC</p>
-        <img alt="icon" src={props.data.icon} />
+    return (
+      <div className="WeatherInfo">
+        <div className="row">
+          <div className="col-6">
+            <h2>{props.data.city}</h2>
+            <span className="temperature">
+              {" "}
+              {Math.round(props.data.temperature)}{" "}
+            </span>
+            <span className="unit"> ºC</span>
+            <img alt="icon" src={props.data.icon} />
+          </div>
+          <div className="col-6">
+            <ul className="text-center">
+              <li className="text-capitalize">
+                {" "}
+                Description: {props.data.description}{" "}
+              </li>
+              <li> Humidity: {Math.round(props.data.humidity)} % </li>
+              <li> Wind: {Math.round(props.data.wind)} Km/h </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="col-6">
-        <ul>
-          <li> Description: {props.data.description} </li>
-          <li> Humidity: {Math.round(props.data.humidity)} % </li>
-          <li> Wind: {Math.round(props.data.wind)} Km/h </li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
 }
