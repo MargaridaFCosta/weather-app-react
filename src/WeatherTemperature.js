fbf0474219632384fcc43717792bbe78e9e976ca
MargaridaFCosta <margaridaxavicosta@hotmail.com>
2023-02-05 19:55:28 +0000

    Update the unit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -21,12 +21,12 @@ export default function WeatherTemperature(props) {
         return (
             <div className="WeatherTemperature">
                 <span className="temperature"> {Math.round(props.celsius)} </span>
-                <span className="unit"> ºC | <a href="/" onclick={showFahrenheit}> ºF </a> </span></div>);
+                <span className="unit"> ºC | <a href="/" onClick={showFahrenheit}> ºF </a> </span></div>);
     } else {
         return (
             <div className="WeatherTemperature">
                 <span className="temperature"> {Math.round(fahrenheit())} </span>
-                <span className="unit"> <a href="/" onclick={showCelsius}> ºC </a> | ºF </span>
+                <span className="unit"> <a href="/" onClick={showCelsius}> ºC </a> | ºF </span>
             </div>
         );
     }
